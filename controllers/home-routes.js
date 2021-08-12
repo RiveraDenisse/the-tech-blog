153d1ff01e4049dce288661a9a020bd460a0877c
Denisse Rivera <denisse_alejandrar@hotmail.com>
2021-08-11 23:02:41 -0500

    work on home-routes, get all posts

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,7 +1,49 @@
 const router = require("express").Router();
-//this is the homepage we want to render
+const sequelize = require("../config/connection");
+const { Post, User, Comment } = require("../models");
+
+//these are routes for the homepage we want to render
 router.get("/", (req, res) => {
-  res.render("homepage");
+  Post.findAll({
+    attributes: [
+      "id",
+      "post_url",
+      "title",
+      "created_at",
+      [
+        sequelize.literal(
+          "(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)"
+        ),
+        "vote_count",
+      ],
+    ],
+    include: [
+      {
+        model: Comment,
+        attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+        include: {
+          model: User,
+          attributes: ["username"],
+        },
+      },
+      {
+        model: User,
+        attributes: ["username"],
+      },
+    ],
+  })
+    .then((dbPostData) => {
+      // pass a single post object into the homepage template
+      console.log(dbPostData[0]);
+      //array for posts
+      const posts = dbPostData.map((post) => post.get({ plain: true }));
+      //pass posts into homepage
+      res.render("homepage", { posts });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
