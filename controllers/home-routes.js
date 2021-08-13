06bd5cee2d2864acec2420d37e1007adc93da146
Denisse Rivera <denisse_alejandrar@hotmail.com>
2021-08-12 22:45:12 -0500

    created login page

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,6 +2,10 @@ const router = require("express").Router();
 const sequelize = require("../config/connection");
 const { Post, User, Comment } = require("../models");
 
+router.get("/login", (req, res) => {
+  res.render("login");
+});
+
 //these are routes for the homepage we want to render
 router.get("/", (req, res) => {
   Post.findAll({
